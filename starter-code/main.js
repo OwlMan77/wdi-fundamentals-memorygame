c6dc6401d4614055a9b941d9a23d41e74ca958f8
Jean-Marc McGhee <JeanMarcMcGhee@Georges-Air-2.home>
2016-10-21 06:29:11 +0100

    added js create card function and experimented with HTML

--- starter-code/main.js
+++ starter-code/main.js
@@ -10,4 +10,17 @@ if (cardOne === cardFour){
 }
 else {
 	alert("Sorry, try again.");
+	}
+
+
+
+var createCards = function(){
+for (i = 0; i <= 4; i++){
+	var GBoard = document.getElementById('game-board')
+	var newCard = document.createElement('div')
+	newCard.className = 'card';
+	GBoard.appendChild(newCard);
+	}
 }
+
+createCards();
