17cec0438821167bdb2d9819348b36d521a37159
Jean-Marc McGhee <JeanMarcMcGhee@Georges-Air-2.home>
2016-10-26 00:37:54 +0100

    Added functions to interact with game board and finish game, added roundcounter and score to reset theboard once all cards are turned over, added delays to ensure cards are turned over first before alerts.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,26 +1,74 @@
 console.log("JS file is connected to HTML! Woo!")
-
 var cardOne = "queen" ;
 var cardTwo = "queen" ;
 var cardThree = "king" ;
 var cardFour = "king";
 
-if (cardOne === cardFour){ 
-	alert("You found a match!");
-}
-else {
-	alert("Sorry, try again.");
-	}
+//if (cardOne === cardFour){ alert("You found a match!");} else {alert("Sorry, try again.");} -->
 
-
-
-var createCards = function(){
-for (i = 0; i <= 4; i++){
-	var GBoard = document.getElementById('game-board')
-	var newCard = document.createElement('div')
+var createBoard = function(){
+for (i = 0; i <= cards.length - 1; i++){
+	var GBoard = document.getElementById('game-board');
+	var newCard = document.createElement('div');
 	newCard.className = 'card';
+	newCard.id = 'card';
 	GBoard.appendChild(newCard);
+	newCard.setAttribute('data-card', cards[i]);
+	newCard.addEventListener('click', isTwoCards);
+	}
+}
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = [];
+// made this to referenced later to clean the board 
+var CardElements = document.getElementsByClassName('card');
+var score = 0;
+//counts the number of matches before the board is cleared
+var roundcount = 0;
+
+
+var isMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+	//delayed the alerts and clear function so that the card img would be come first.
+	setTimeout(function(){alert("You found a match!");}, 200);
+	//Updates score Variable and updates it in the html 
+	score = score + 1;
+	document.getElementById("Score").innerHTML = score;
+	roundcount = roundcount + 1;
+	}
+	else {
+	setTimeout(function()
+		{alert("Sorry, try again.");
+		clearCards();
+		}, 200);
+	}
+
+}
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'))
+	if (this.getAttribute('data-card') === "king") {
+	this.innerHTML = '<img src ="king.png" alt="KingHalf"/>';
+		}
+	else {this.innerHTML ='<img src ="queen.png" alt="QueenHalf"/>'}
+
+	// makes sure that only 2 cards are being matched at the same time.
+	if (cardsInPlay.length === 2) {
+	isMatch(cardsInPlay);
+	cardsInPlay =[];
+	}
+	//clears the board once all matches are found.
+	if (roundcount != 0 && roundcount % 2 == 0){
+	setTimeout(function(){clearCards();},200);
+	roundcount = 0;
 	}
 }
 
-createCards();
+// Clears card imgs from card divs
+var clearCards = function(){
+	for (i = 0; i < cards.length; i++){
+	CardElements[i].innerHTML ='';
+}
+	}
+
+//sets up the board for the game
+createBoard();
